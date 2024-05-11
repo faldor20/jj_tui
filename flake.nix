dabe9ceb9729400fa6805ed943aed91274dd20e9
Eli Dowling <eli.jambu@gmail.com>
2024-05-11 15:31:54 +1000

    try using nix

--- flake.nix
+++ flake.nix
@@ -1,55 +1,81 @@
 {
   inputs = {
-    opam-nix.url = "github:tweag/opam-nix";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.follows = "opam-nix/nixpkgs";
+    nixpkgs.url = "nixpkgs-unstable";
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs.opam-repository.follows = "opam-repository";
+    };
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+
+  outputs = { self, flake-utils, opam-nix, opam-repository, nixpkgs, ... }@inputs:
+    let
+      package = "jj_tui";
+      overlay = final: prev: {
+        ${package} = prev.${package}.overrideAttrs (_: {
+          # Do not add share/nix-support, so that dependencies from
+          # the scope don't leak into dependent derivations
+          doNixSupport = false;
+        });
+        dune-release = prev.dune-release.overrideAttrs (_: {
+          doCheck = false;
+        });
+      };
+    in
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { overlays = [ overlay ]; inherit system; };
         on = opam-nix.lib.${system};
-        localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
-          (on.listRepo (on.makeOpamRepo ./.));
-        devPackagesQuery = {
-          # You can add "development" packages here. They will get added to the devShell automatically.
-          ocaml-lsp-server = "*";
-          ocamlformat = "*";
+        localPackages = {
+        eio-process="*";
+        jj_tui="*";
+        notty="*";
         };
-        query = devPackagesQuery // {
-          ## You can force versions of certain packages here, e.g:
-          ## - force the ocaml compiler to be taken from opam-repository:
-          ocaml-base-compiler = "5.1.1";
-          ## - or force the compiler to be taken from nixpkgs and be a certain version:
-          # ocaml-system = "5.1.1";
-          ## - or force ocamlfind to be a certain version:
-          # ocamlfind = "1.9.2";
+        devPackages = {
+          ppx_yojson_conv = "*";
+          ppx_expect = "*";
         };
-        scope = on.buildOpamProject' { } ./. query;
-        overlay = final: prev:
-          {
-            # You can add overrides here
-          };
-        scope' = scope.overrideScope' overlay;
-        # Packages from devPackagesQuery
-        devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
-        # Packages in this workspace
+        packagesFromNames = set:
+          (builtins.map (s: builtins.getAttr s scope)
+            (builtins.attrNames set));
+        allPackages = localPackages // devPackages;
+        scope =
+          (
+            let
+              scope =
+                on.buildDuneProject
+                  {
+                    repos = [ opam-repository ];
+                    inherit pkgs;
+                    resolveArgs = { with-test = true; };
+                  }
+                  package
+                  ./.
+                  (allPackages);
+            in
+            scope.overrideScope' overlay
+          );
+      in
+      {
         packages =
-          pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope';
-      in {
-        legacyPackages = scope';
+          (nixpkgs.lib.filterAttrs (name: value: builtins.hasAttr name localPackages) scope) //
+          { default = self.packages.${system}.${package}; };
 
-        # inherit packages;
-
-        ## If you want to have a "default" package which will be built with just `nix build`, do this instead of `inherit packages;`:
-        packages = packages // { default = packages.jj_tui; };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues packages;
-          buildInputs = devPackages ++ [
-            # You can add packages from nixpkgs here
-          ];
-        };
+        devShell =
+          pkgs.mkShell {
+            buildInputs = (with pkgs;
+              [
+                # dev tools
+                ocamlformat_0_24_1
+                yarn
+                dune-release
+              ]) ++ packagesFromNames devPackages;
+            inputsFrom = [ self.packages.${system}.default ]
+              ++ packagesFromNames localPackages;
+          };
       });
 }
