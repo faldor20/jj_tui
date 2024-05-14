e2aa2aaab27df1638f441997c970ce9dc641428d
Eli Dowling <eli.jambu@gmail.com>
2024-05-14 16:37:53 +1000

    add nix build

--- flake.nix
+++ flake.nix
@@ -14,79 +14,93 @@
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
+          jj_tui = pkgs: ocamlPackages: profile:
+
+            let
+              eio-process = ocamlPackages.buildDunePackage {
+                pname = "eio-process";
+                version = "0.1.0";
+                duneVersion = "3";
+                src = pkgs.fetchFromGitHub {
+
+                  owner = "mbarbin";
+                  repo = "eio-process";
+                  rev = "482ba341884dc8711f93ec9cc6d7c941099e0faa";
+                  sha256 =
+                    "sha256-/Y2U+1y+nDMBrRfDAYif0WJp0vPWmvbSMt39wAB/rS8=";
+                };
+
+                buildInputs = with ocamlPackages; [
+                  base
+                  eio
+                  parsexp
+                  ppx_compare
+                  ppx_enumerate
+                  ppx_hash
+                  ppx_here
+                  ppx_let
+                  ppx_sexp_conv
+                  ppx_sexp_value
+                ];
+
+                strictDeps = true;
+              };
+              jj_tui_build_pkgs =
+
+                [
+                  eio-process
+                  ocamlPackages.parsexp
+                  ocamlPackages.eio_main
+                  ocamlPackages.stdio
+                  ocamlPackages.base
+                  ocamlPackages.angstrom
+                  ocamlPackages.ppx_expect
+                  ocamlPackages.uutf
+                  # ocamlPackages.parsexp
+
+                  # Ocaml package dependencies needed to build go here.
+                ];
+              jj_tui = let pname = "jj_tui";
+              in ocamlPackages.buildDunePackage {
+                pname = pname;
+                version = "0.1.0";
+                duneVersion = "3";
+                src = ./.;
+
+                buildInputs = jj_tui_build_pkgs;
+
+
+                buildPhase = ''
+                  runHook preBuild
+                  dune build -p ${pname} --profile ${profile}  ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
+                  runHook postBuild
+                '';
+                checkPhase = ''
+                  runHook preCheck
+                  dune runtest -p ${pname}--profile ${profile} ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
+                  runHook postCheck
+                '';
+                installPhase = ''
+                  runHook preInstall
+                  dune install --profile ${profile} --prefix $out --libdir $OCAMLFIND_DESTDIR ${pname}
+                  runHook postInstall
+                '';
+
+              };
+            in {
+              jj_tui = jj_tui;
+              jj_tui_build_pkgs = jj_tui_build_pkgs;
+            };
           # OCaml packages available on nixpkgs
           inherit (pkgs) ocamlPackages mkShell lib;
           inherit (ocamlPackages) buildDunePackage;
-          ocamlPkgssStatic =
-            pkgs.pkgsMusl.ocamlPackages; # Legacy packages that have not been converted to flakes
-          eio-process = buildDunePackage {
-            pname = "eio-process";
-            version = "0.1.0";
-            duneVersion = "3";
-            src = pkgs.fetchFromGitHub {
-
-              owner = "mbarbin";
-              repo = "eio-process";
-              rev = "482ba341884dc8711f93ec9cc6d7c941099e0faa";
-              sha256 = "sha256-/Y2U+1y+nDMBrRfDAYif0WJp0vPWmvbSMt39wAB/rS8=";
-            };
-
-            buildInputs = with ocamlPackages; [
-              base
-              eio
-              parsexp
-              ppx_compare
-              ppx_enumerate
-              ppx_hash
-              ppx_here
-              ppx_let
-              ppx_sexp_conv
-              ppx_sexp_value
-            ];
-
-            strictDeps = true;
-          };
-          jj_tui_build_pkgs =
-
-            [
-              eio-process
-              ocamlPackages.parsexp
-              ocamlPackages.eio_main
-              ocamlPackages.stdio
-              ocamlPackages.base
-              ocamlPackages.angstrom
-              ocamlPackages.ppx_expect
-              ocamlPackages.uutf
-              # ocamlPackages.parsexp
-
-              # Ocaml package dependencies needed to build go here.
-            ];
-          jj_tui = pkgs: ocamlPackages:
-            ocamlPackages.buildDunePackage {
-              pname = "jj_tui";
-              version = "0.1.0";
-              duneVersion = "3";
-              src = ./.;
-
-              buildInputs = jj_tui_build_pkgs;
-              env = {
-                DUNE_PROFILE = "static";
-
-              };
-
-              strictDeps = true;
-
-              preBuild = ''
-                export DUNE_PROFILE=static
-              '';
-            };
 
         in {
           packages = {
-            default =
-              jj_tui pkgs ocamlPackages; # Development environment output
-            pkgsMusl.default = jj_tui pkgs.pkgsMusl
-              pkgs.pkgsMusl.ocamlPackages; # Development environment output
+            default = (jj_tui pkgs
+              ocamlPackages "release").jj_tui; # Development environment output
+            static = (jj_tui pkgs.pkgsMusl
+              pkgs.pkgsMusl.ocamlPackages "static").jj_tui; # Development environment output
           };
           devShells = {
             default = mkShell.override { stdenv = pkgs.gccStdenv; } {
