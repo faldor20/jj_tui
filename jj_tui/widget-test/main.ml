d0aa4cef74ffb27f0466967c8dfc21c3be791218
faldor20 <eli.jambu@gmail.com>
2024-05-24 21:13:29 +1000

    more tests

--- jj_tui/widget-test/main.ml
+++ jj_tui/widget-test/main.ml
@@ -92,19 +92,29 @@ let w_2 =
 ;;
 
 let renderer = Renderer.make ()
+let navMode ui = ui
+(* |>$ Ui.keyboard_area (function *)
+(* | `Arrow key, mods -> *)
+(* let dir : [ `Down | `Left | `Right | `Up ] :> *)
+(* [ `Down | `Left | `Right | `Up | `Next | `Prev ] *)
+(* = *)
+(* key *)
+(* in *)
+(* Renderer.dispatch_key renderer (`Focus dir, mods) *)
+(* | _ -> *)
+(* `Unhandled) *)
 
-let navMode ui =
-  ui
-  (* |>$ Ui.keyboard_area (function *)
-    (* | `Arrow key, mods -> *)
-      (* let dir : [ `Down | `Left | `Right | `Up ] :> *)
-        (* [ `Down | `Left | `Right | `Up | `Next | `Prev ] *)
-        (* = *)
-        (* key *)
-      (* in *)
-      (* Renderer.dispatch_key renderer (`Focus dir, mods) *)
-    (* | _ -> *)
-      (* `Unhandled) *)
+let is_focused_widget () =
+  let focus = Focus.make () in
+  let$ focus = focus |> Focus.status in
+  W.string (if focus |> Focus.has_focus then "focused " else "not_focused")
+  |> Ui.keyboard_area ~focus (fun x -> `Unhandled)
+;;
+
+let is_focused_widget2 ~focus =
+  let$ focus = focus |> Focus.status in
+  W.string (if focus |> Focus.has_focus then "focused " else "not_focused")
+  |> Ui.keyboard_area ~focus (fun x -> `Unhandled)
 ;;
 
 let w_3 =
@@ -112,8 +122,9 @@ let w_3 =
   Focus.request start;
   W.hbox
     [
-      W.vbox
+      Wd.v_window_stack2
         [
+          is_focused_widget ();
           W.string "hi this is the first level"
           |> Lwd.pure
           |> W.scroll_area
@@ -127,21 +138,61 @@ let w_3 =
               W.string "hi this is the second level"
               |> Lwd.pure
               |> W.scroll_area
-              |> Wd.border_box_focusable;
+              |> Wd.border_box_focusable ~focus:start;
+              Wd.v_window_stack2
+                [
+                  W.string "hi this is the first level"
+                  |> Lwd.pure
+                  |> W.scroll_area
+                  |> Wd.border_box_focusable;
+                  W.string "hi this is the second level"
+                  |> Lwd.pure
+                  |> W.scroll_area
+                  |> Wd.border_box_focusable;
+                ];
             ];
         ];
-      W.vbox
+      Wd.v_window_stack2
         [
           W.string "hi this is the first level"
           |> Lwd.pure
           |> W.scroll_area
-          |> Wd.border_box_focusable ~focus:start;
+          |> Wd.border_box_focusable;
           W.string "hi this is the second level"
           |> Lwd.pure
           |> W.scroll_area
           |> Wd.border_box_focusable;
         ];
-    ]|>navMode
+    ]
+  |> navMode
+;;
+
+let w_5 =
+  let focus = Focus.make () in
+  Focus.request focus;
+  Wd.v_window_stack
+    ~focus
+    [
+      is_focused_widget2;
+      is_focused_widget2;
+      Wd.h_window_stack
+        [
+          is_focused_widget2;
+          is_focused_widget2;
+          Wd.v_window_stack
+            [
+              is_focused_widget2;
+              is_focused_widget2;
+              Wd.h_window_stack
+                [
+                  is_focused_widget2;
+                  is_focused_widget2;
+                  Wd.v_window_stack [ is_focused_widget2; is_focused_widget2 ];
+                ];
+            ];
+        ];
+      Wd.h_window_stack [ is_focused_widget2 ];
+    ]
 ;;
 
 let w_4 =
@@ -169,6 +220,45 @@ let w_4 =
       `Unhandled)
 ;;
 
+let w_6 =
+  let state = Lwd.var W.default_scroll_state in
+  W.vbox
+    [
+      Ui.vcat [ W.string "hi";W.string "50"; W.string "there"; W.string "mate" ]
+      |> Lwd.pure
+      |> W.vscroll_area ~change:(fun _ x -> state $= x) ~state:(Lwd.get state)
+      |>$ Ui.resize ~h:2 ~sh:0;
+      Wd.h_rule |> Lwd.pure;
+      W.file_select ~on_select:(fun x -> ()) ();
+      W.unfoldable
+        (W.string "click to unfold" |> Lwd.pure)
+        (fun () -> W.string "I'm unfolded" |> Lwd.pure);
+    ]
+;;
+let w_7 =
+  let state = Lwd.var (W.default_scroll_state,W.default_scroll_state) in
+  W.vbox
+    [
+    Wd.h_window_stack2[
+      Ui.vcat [ W.string "hi";W.string "50"; W.string "there345678987654356789876545end"; W.string "mate" ]
+      |> Lwd.pure
+      |> Wd.scroll_area 
+      |>$ Ui.resize ~mw:(15)
+      ;
+      W.string "|"|>Lwd.pure
+      ];
+    Wd.h_window_stack2[
+      Ui.vcat [ W.string "hi";W.string "50"; W.string "there345678987654356789876545end"; W.string "mate" ]
+      |> Lwd.pure
+      |> Wd.v_scroll_area 
+      |>$ Ui.resize  ~h:2 ~sh:(0)
+      ;
+      W.string "|"|>Lwd.pure
+      ];
+      W.string "----"|>Lwd.pure
+    ]
+;;
+
 let quit = Lwd.var false
 
 let main_ui =
@@ -185,6 +275,12 @@ let main_ui =
      w_3
    | 4 ->
      w_4
+   | 5 ->
+     w_5
+   | 6 ->
+     w_6
+   | 7->
+     w_7
    | _ ->
      W.string "not a test" |> Lwd.pure)
   |>$ Ui.event_filter (function
@@ -200,7 +296,16 @@ let main_ui =
     | `Key (`ASCII '4', _) ->
       Lwd.set test_number 4;
       `Handled
-    | `Key (`ASCII 'q', [`Ctrl]) ->
+    | `Key (`ASCII '5', _) ->
+      Lwd.set test_number 5;
+      `Handled
+    | `Key (`ASCII '6', _) ->
+      Lwd.set test_number 6;
+      `Handled
+    | `Key (`ASCII '7', _) ->
+      Lwd.set test_number 7;
+      `Handled
+    | `Key (`ASCII 'q', [ `Ctrl ]) ->
       quit $= true;
       `Handled
     | _ ->
