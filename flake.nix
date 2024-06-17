961732806570d780995fba45fcac67c9f6a1050a
faldor20 <eli.jambu@gmail.com>
2024-06-17 10:53:45 +1000

    using raw nix for packaging

--- flake.nix
+++ flake.nix
@@ -1,70 +1,148 @@
 {
+  description = "Example JavaScript development environment for Zero to Nix";
+
+  # Flake inputs
   inputs = {
-    opam-repository = {
-      url = "github:ocaml/opam-repository";
-      flake = false;
-    };
-    nixpkgs-unstable.url = "nixpkgs-unstable";
-    opam-nix = {
-      url = "github:tweag/opam-nix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-      inputs.opam-repository.follows = "opam-repository";
 
+    nixpkgs.url = "nixpkgs-unstable"; # also valid: "nixpkgs"
+
+    ocaml-overlay = {
+      url = "github:faldor20/nix-overlays";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.follows = "nixpkgs-unstable";
   };
-  outputs =
-    { self, flake-utils, opam-nix, nixpkgs, opam-repository, ... }@inputs:
-    # Don't forget to put the package name instead of `throw':
-    let package = "jj_tui";
-    in flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        on = opam-nix.lib.${system};
-        devPackagesQuery = {
-          # You can add "development" packages here. They will get added to the devShell automatically.
-          # ocaml-lsp-server = "*";
-          # ocamlformat = "*";
-        };
-        query = devPackagesQuery // {
-          ## You can force versions of certain packages here, e.g:
-          ## - force the ocaml compiler to be taken from opam-repository:
-          # ocaml-base-compiler = "5.1.1";
-          ## - or force the compiler to be taken from nixpkgs and be a certain version:
-          ocaml-system = "5.1.1";
-          ## - or force ocamlfind to be a certain version:
-          # ocamlfind = "1.9.2";
-        };
-        scope = on.buildDuneProject {
-          pkgs = pkgs.pkgsStatic;
-        } package ./. query;
+  # Flake outputs
+  outputs = { self, nixpkgs, flake-parts, ocaml-overlay, ... }@inputs:
 
-        overlay = final: prev: {
-          # You can add overrides here
-          ${package} = prev.${package}.overrideAttrs (_: {
-            env = { DUNE_PROFILE = "static"; };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          jj_tui = pkgs: ocamlPackages: profile:
 
-            # Prevent the ocaml dependencies from leaking into dependent environments
-            doNixSupport = false;
-          });
+            let
+              notty = ocamlPackages.notty.overrideAttrs
+                (old: { src = ./forks/notty/.; });
+              eio-process = ocamlPackages.buildDunePackage {
+                pname = "eio-process";
+                version = "0.1.0";
+                duneVersion = "3";
+                src = pkgs.fetchFromGitHub {
+
+                  owner = "mbarbin";
+                  repo = "eio-process";
+                  rev = "482ba341884dc8711f93ec9cc6d7c941099e0faa";
+                  sha256 =
+                    "sha256-/Y2U+1y+nDMBrRfDAYif0WJp0vPWmvbSMt39wAB/rS8=";
+                };
+
+                buildInputs = with ocamlPackages; [
+                  notty
+                  base
+                  eio
+                  parsexp
+                  ppx_compare
+                  ppx_enumerate
+                  ppx_hash
+                  ppx_here
+                  ppx_let
+                  ppx_sexp_conv
+                  ppx_sexp_value
+                ];
+
+                strictDeps = true;
+              };
+              jj_tui_build_pkgs =
+
+                [
+                  eio-process
+                  ocamlPackages.parsexp
+                  ocamlPackages.eio_main
+                  ocamlPackages.stdio
+                  ocamlPackages.base
+                  ocamlPackages.angstrom
+                  ocamlPackages.ppx_expect
+                  ocamlPackages.uutf
+                  # ocamlPackages.parsexp
+
+                  # Ocaml package dependencies needed to build go here.
+                ];
+              jj_tui = let pname = "jj_tui";
+              in ocamlPackages.buildDunePackage {
+                pname = pname;
+                version = "0.1.0";
+                duneVersion = "3";
+                src = ./.;
+
+                buildInputs = jj_tui_build_pkgs;
+
+                buildPhase = ''
+                  runHook preBuild
+                  dune build -p ${pname} --profile ${profile}  ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
+                  runHook postBuild
+                '';
+                checkPhase = ''
+                  runHook preCheck
+                  dune runtest -p ${pname}--profile ${profile} ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
+                  runHook postCheck
+                '';
+                installPhase = ''
+                  runHook preInstall
+                  dune install --profile ${profile} --prefix $out --libdir $OCAMLFIND_DESTDIR ${pname}
+                  runHook postInstall
+                '';
+
+              };
+            in {
+              jj_tui = jj_tui;
+              jj_tui_build_pkgs = jj_tui_build_pkgs;
+            };
+          # OCaml packages available on nixpkgs
+          ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_1;
+          inherit (pkgs) mkShell lib;
+
+        in {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              ocaml-overlay.overlays.default
+
+              # (self: super: {
+              #   ocaml-ng.ocamlPackages_5_1.logs =
+              #     (super.ocaml-ng.ocamlPackages_5_1.logs.override { jsooSupport = false; }).overrideAttrs
+              #     (o: {
+              #       pname = "logs";
+              #       propagatedBuildInputs = o.propagatedBuildInputs
+              #         ++ [ super.ocaml-ng.ocamlPackages_5_1.topkg ];
+              #     });
+              # })
+            ];
+          };
+          packages = {
+            default = (jj_tui pkgs ocamlPackages
+              "release").jj_tui; # Development environment output
+            static =
+              (jj_tui pkgs.pkgsStatic pkgs.pkgsStatic.ocaml-ng.ocamlPackages_5_1
+                "static").jj_tui; # Development environment output
+          };
+          devShells = {
+            default = mkShell.override { stdenv = pkgs.gccStdenv; } {
+              buildInputs = with ocamlPackages; [
+                dune_3
+                ocaml
+                utop
+                ocamlformat
+              ];
+              inputsFrom = [ self'.packages.default ];
+              packages = builtins.attrValues {
+                inherit (pkgs) gcc pkg-config;
+                inherit (ocamlPackages) ocaml-lsp ocamlformat-rpc-lib;
+              };
+            };
+          };
+
         };
-        scope' = scope.overrideScope' overlay;
-        # The main package containing the executable
-        main = scope'.${package};
-        # Packages from devPackagesQuery
-        devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
-      in {
-        legacyPackages = scope';
-
-        packages.default = main;
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ main ];
-          buildInputs = devPackages ++ [
-            # You can add packages from nixpkgs here
-          ];
-        };
-      });
+    };
 }
